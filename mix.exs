f05028326e48160ee6961bb50e903850ac619a5d
Kian-Meng, Ang <kianmeng@cpan.org>
2021-08-11 21:10:05 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,46 +1,55 @@
 defmodule ExDocDash.Mixfile do
-	use Mix.Project
+  use Mix.Project
 
-	def project do
-		[
-			app: :ex_doc_dash,
-			version: "0.3.1",
-			elixir: "~> 1.2",
-			deps: deps,
-			description: description,
-			package: package,
-			name: "ExDocDash",
-			docs: [extras: ["README.md"]],
-			source_url: "https://github.com/JonGretar/ExDocDash",
-			homepage_url: "http://hexdocs.pm/ex_doc_dash"
-		]
-	end
+  @source_url "https://github.com/JonGretar/ExDocDash"
+  @version "0.3.1"
 
-	def application do
-		[applications: [:logger]]
-	end
+  def project do
+    [
+      app: :ex_doc_dash,
+      name: "ExDocDash",
+      version: @version,
+      elixir: "~> 1.2",
+      deps: deps(),
+      docs: docs(),
+      package: package()
+    ]
+  end
 
-	defp deps do
-		[
-			{:ex_doc, ">= 0.12.0"}
-		]
-	end
+  def application do
+    [applications: [:logger]]
+  end
 
-	defp description do
-		"""
-		Formatter for ExDoc to generate docset documentation for use in Dash.app.
-		"""
-	end
+  defp deps do
+    [
+      {:ex_doc, ">= 0.0.0", runtime: false}
+    ]
+  end
 
-	defp package do
-		[
-			maintainers: ["Jón Grétar Borgþórsson"],
-			licenses: ["MIT"],
-			links: %{
-				"Dash.app": "http://kapeli.com/dash",
-				"GitHub": "https://github.com/JonGretar/ExDocDash",
-				"Issues": "https://github.com/JonGretar/ExDocDash/issues"
-			}
-		]
-	end
+  defp package do
+    [
+      description: "Formatter for ExDoc to generate docset documentation for use in Dash.app.",
+      maintainers: ["Jón Grétar Borgþórsson"],
+      licenses: ["MIT"],
+      links: %{
+        "Dash.app": "https://kapeli.com/dash",
+        GitHub: @source_url,
+        Issues: "#{@source_url}/issues"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version,
+      homepage_url: @source_url,
+      formatters: ["html"]
+    ]
+  end
 end
